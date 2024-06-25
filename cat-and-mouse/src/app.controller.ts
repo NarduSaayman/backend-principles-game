7b0507335dff1aa1b32686986553fb657ec5e605
NarduSaayman <nsaayman@jhb.dvt.co.za>
2024-06-25 14:15:49 +0200

    feat: :sparkles: Added Fastify to NestJS

--- cat-and-mouse/src/app.controller.ts
+++ cat-and-mouse/src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHelloFastify(): string {
+    return this.appService.getHelloFastify();
   }
 }
