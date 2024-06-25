ce013c29deadcd9855da09f6aba3ea74160505af
NarduSaayman <nsaayman@jhb.dvt.co.za>
2024-06-25 14:22:43 +0200

    test: :white_check_mark: pass hello fastify test

--- cat-and-mouse/src/app.controller.spec.ts
+++ cat-and-mouse/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHelloFastify()).toBe('Hello World!');
+    it('should return "Hello Fastify!"', () => {
+      expect(appController.getHelloFastify()).toBe('Hello Fastify!');
     });
   });
 });
